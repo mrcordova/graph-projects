e3d608328d81db1404cb419fd8493ecbb58bc210
Noah Cordova <noahco86@gmail.com>
2022-04-13 16:09:44 -0700

    add resource for dataset.json

--- chart/chart/Model/chartData.swift
+++ chart/chart/Model/chartData.swift
@@ -23,15 +23,21 @@ struct ChartData: Codable {
 
 func load() -> ChartData {
     
-    let fm = FileManager.default
-    let home = fm.homeDirectoryForCurrentUser
-    let dataPathUrl = home
-        .appendingPathComponent("Desktop")
-        .appendingPathComponent("DataSet")
-        .appendingPathExtension("json")
+//    let fm = FileManager.default
+//    let home = fm.homeDirectoryForCurrentUser
+    guard let dataPathUrl = Bundle.main.path(forResource: "DataSet", ofType: "json"),
+        
+    let dataSet = FileManager.default.contents(atPath: dataPathUrl) else
+    {
+        fatalError("Path url: failed")
+    }
+//    let dataPathUrl = home
+//        .appendingPathComponent("Desktop")
+//        .appendingPathComponent("DataSet")
+//        .appendingPathExtension("json")
     
     do {
-        let contents = try Data(contentsOf: dataPathUrl)
+        let contents = dataSet
         let decoder = JSONDecoder()
         return try decoder.decode(ChartData.self, from: contents)
     } catch {
