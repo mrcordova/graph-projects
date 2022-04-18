360f2c92fd97d7afd1fdc18217a09c2af99d4cc9
Noah Cordova <noahco86@gmail.com>
2022-04-18 09:41:05 -0700

    remove dead code

--- chart/chart/Model/ChartData.swift
+++ chart/chart/Model/ChartData.swift
@@ -22,19 +22,14 @@ struct ChartData: Codable {
 }
 
 func load() -> ChartData {
-    
-//    let fm = FileManager.default
-//    let home = fm.homeDirectoryForCurrentUser
+ 
     guard let dataPathUrl = Bundle.main.path(forResource: "DataSet", ofType: "json"),
         
     let dataSet = FileManager.default.contents(atPath: dataPathUrl) else
     {
         fatalError("Path url: failed")
     }
-//    let dataPathUrl = home
-//        .appendingPathComponent("Desktop")
-//        .appendingPathComponent("DataSet")
-//        .appendingPathExtension("json")
+
     
     do {
         let contents = dataSet
